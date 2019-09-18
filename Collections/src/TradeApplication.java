9b735fe48404310264380e622b3d69b640497071
Binaries <Binaries@Binariess-MacBook-Pro.local>
2019-09-18 08:32:56 +0530

    Commit for List Code

--- Collections/src/TradeApplication.java
+++ Collections/src/TradeApplication.java
@@ -10,56 +10,68 @@ public class TradeApplication {
 
     public static void main(String[] args) {
 
+        // 1st Object
         Fund fund = new Fund();
         fund.setFundName("ABC Fund");
         fund.setFundNumber(123);
 
         Trade t = new FxTrade();
+
         t.setFund(fund);
         t.setTradeAmount(120.12);
         t.setNoOfFunds(1);
         t.setTradeId(1234);
         t.setTraderName("Trader");
-        // this will not work --> t1.setCurrency("EURO");
+        // t.setCurrency("EURO"); --> this will not work. Why?
         // So use parameterized constructor
         t.showTradeDetails();
 
-
-
+        // 2nd Object
+        Fund _fund = new Fund();
+        _fund.setFundName("ABC Fund with Underscore");
+        _fund.setFundNumber(345);
 
         Trade _t = new FxTrade("EURO");
-        _t.setFund(fund);
+
+        _t.setFund(_fund);
         _t.setTradeAmount(130.12);
         _t.setNoOfFunds(1);
         _t.setTradeId(1234);
         _t.setTraderName("Trader _");
         _t.showTradeDetails();
 
+        // 3rd Object
+        Fund fund1 = new Fund();
+        fund1.setFundName("ABC Fund with Number 1");
+        fund1.setFundNumber(567);
 
-       Trade t1 = new FxTrade(12345, "Trader 1", 140.12, 1, "EURO" );
-       t1.setFund(fund);
-       t1.showTradeDetails();
+        Trade t1 = new FxTrade(12345, "Trader 1", 140.12, 1, "EURO");
 
-       List<Trade> tradeList = new ArrayList<>();
-       tradeList.add(t);
-       tradeList.add(t1);
-       tradeList.add(_t);
+        t1.setFund(fund1);
 
-       // Trade List is ready
+        t1.showTradeDetails();
 
-       // Iterate using iterator
+        List<Trade> tradeList = new ArrayList<>();
+        tradeList.add(t);
+        tradeList.add(t1);
+        tradeList.add(_t);
+
+        // Trade List is ready, Lets iterate!
+
+        // Iterate using iterator
         System.out.println("Iterating using iterator...");
         Iterator itr = tradeList.iterator(); // iterating
         while (itr.hasNext()) {
             // Object obj = itr.next();
             // obj. --> You wont get Trade methods unless you cast it
-            Trade tradeObj = (Trade)itr.next(); // This is called casting when you use () in front
+            Trade tradeObj = (Trade) itr.next(); // This is called casting when you use () in front
             tradeObj.showTradeDetails();
             System.out.println(tradeObj.toString());
         }
+
         // Iterate using for loop
         System.out.println("Iterating using for loop...");
-        for (Trade trade: tradeList){
+        for (Trade trade : tradeList) {
             trade.showTradeDetails();
             System.out.println(trade.toString());
         }
